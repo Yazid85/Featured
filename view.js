97264c2ccbba209443c0a432d4eee49e35acf30b
Yazid85 <81072709+Yazid85@users.noreply.github.com>
2026-08-29 13:29:52 +0700

    Update view.js

--- view.js
+++ view.js
@@ -17,6 +17,13 @@
     
     // Huruf Kecil
     if (useAbbr === 1) {
+      if (num >= 1000000) return (num / 1000000).toFixed(1).replace('.', ',') + 'm';
+      if (num >= 1000) return (num / 1000).toFixed(1).replace('.', ',') + 'k';
+      return num.toString();
+    }
+    
+    // Huruf Besar
+    if (useAbbr === 2) {
       if (num >= 1000000) return (num / 1000000).toFixed(1).replace('.', ',') + 'M';
       if (num >= 1000) return (num / 1000).toFixed(1).replace('.', ',') + 'K';
       return num.toString();
